26b1e8a97b3b87e7caad05d30c6b0e63c68622a1
Philou <philippe.bourgau@free.fr>
2014-04-08 07:02:19 +0200

    Fix instabilities on offline specs

--- spec/lib/auchandirect/scrAPI/offline_items_spec.rb
+++ spec/lib/auchandirect/scrAPI/offline_items_spec.rb
@@ -64,28 +64,35 @@ module Auchandirect
       def search_through_files_for(grep_hint, selector)
         `find #{@offline_store_dir} -name *.html -exec grep -l "#{grep_hint}" {} \\;`.split("\n").lazy.
 
-          map do |file|
+        map do |file|
           doc = Nokogiri::HTML(open(file))
           [file, doc.search(selector)]
         end.
 
-          select do |file, elements|
+        select do |file, elements|
           !elements.empty?
         end.
 
-          map do |file, elements|
-          # example : http://www.auchandirect.fr/petit-dejeuner-epicerie-sucree/chocolats,-confiseries/barres-biscuitees-muesli---cereales/id1/485/53869
-
-          file = URI.unescape(file)
-          file = file.gsub(/^.*\/www\.auchandirect\.fr\//, "")
-          pieces = file.split("/").take(3)
-          pieces[2] = pieces[2].split(",")[0]
-          pieces = pieces.map do |piece|
-            piece.gsub(/lv_/,'').split(/[\-_, \(\)']+/).select {|p| not (p.size <= 2 or p =~ /^[0-9]/)}
-          end
-
-          {cat_tokens: pieces[0], sub_cat_tokens: pieces[1], item_tokens: pieces[2], elements: elements}
+        map do |file, elements|
+          split_url_to_tokens(file).merge(elements: elements)
         end
+      end
+
+      def split_url_to_tokens(file)
+        # example : http://www.auchandirect.fr/petit-dejeuner-epicerie-sucree/chocolats,-confiseries/barres-biscuitees-muesli---cereales/id1/485/53869
+
+        file = URI.unescape(file)
+        file = file.gsub(/^.*\/www\.auchandirect\.fr\//, "")
+
+        pieces = file.split("/").take(3)
+
+        pieces[2] = pieces[2].split(",")[0]
+
+        pieces = pieces.map do |piece|
+          piece.gsub(/lv_/,'').split(/[\-_, \(\)']+/).select {|p| not (p.size <= 2 or p =~ /^[0-9]/)}
+        end
+
+        {cat_tokens: pieces[0], sub_cat_tokens: pieces[1], item_tokens: pieces[2]}
       end
 
 
@@ -93,7 +100,7 @@ module Auchandirect
         filter(store.categories, item_info[:cat_tokens]).each do |cat|
           filter(cat.categories, item_info[:sub_cat_tokens]).each do |sub_cat|
             sub_cat.categories.each do |sub_sub_cat|
-              filter(sub_sub_cat.items, item_info[:item_tokens]).each do |item|
+              take_singleton(filter(sub_sub_cat.items, item_info[:item_tokens])).each do |item|
                 return item_info.merge(item: item)
               end
             end
@@ -108,6 +115,24 @@ module Auchandirect
             element.attributes[:name].downcase.include?(token.downcase)
           end
         end
+      end
+
+      def take_singleton(items)
+        item = nil
+
+        begin
+          item = items.next
+        rescue StopIteration
+          return []
+        end
+
+        begin
+          items.next
+        rescue StopIteration
+          return [item]
+        end
+
+        return []
       end
     end
   end
